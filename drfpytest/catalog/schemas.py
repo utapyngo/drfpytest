6c5cec9053d2390093e2294970fb1433f5c32e38
Ivan <ut@pyngo.tom.ru>
2024-01-16 11:50:43 +0700

    Ensure compatibility with older Python versions
    Downgrade Django and other dependencies

--- drfpytest/catalog/schemas.py
+++ drfpytest/catalog/schemas.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -6,5 +10,5 @@ class ProductBrandData(BaseModel):
 
 
 class ProductData(BaseModel):
-    brand: ProductBrandData | None = None
-    size: str | None = None
+    brand: Optional[ProductBrandData] = None
+    size: Optional[str] = None
